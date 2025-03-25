eb9ec74a7ecd276318845c9a10651b24797b8acd
mizuki.sato <mizuki.sato@brainpad.co.jp>
2025-03-25 17:45:02 +0900

    [add]アプリケーションサービスの追加

---
+++ src/aplication/template_service.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TemplateService:
+    def __init__(self) -> None:
+        pass
+
+    def handle(self) -> None:
+        logger.info("このクラスはテンプレートです")
